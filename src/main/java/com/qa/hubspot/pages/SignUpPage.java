2ac60513cdb7781ef08cb9dffaa8b212e341ff25
Khaja ZakiUddin <zakiuddink@yahoo.com>
2022-03-07 13:44:41 +0530

    Create SignUpPage.java
    Added SignUp Page

---
+++ src/main/java/com/qa/hubspot/pages/SignUpPage.java
@@ -0,0 +1,15 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.hubspot.base.BasePage;
+
+public class SignUpPage extends BasePage {
+
+	WebDriver driver;
+
+	public SignUpPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
